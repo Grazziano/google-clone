4e8e490eeb07aa82699fdabca88a0514e84cce5d
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2023-11-12 19:07:06 -0300

    Add home header component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,9 @@
+import HomeHeader from '@/components/HomeHeader';
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Home</h1>
-    </div>
+    <>
+      <HomeHeader />
+    </>
   );
 }
