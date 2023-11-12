6e5d2ab01acf603726b85e2b301e80a91b5b49f3
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2023-11-12 20:07:47 -0300

    Add body section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,24 @@
 import HomeHeader from '@/components/HomeHeader';
+import HomeSearch from '@/components/HomeSearch';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <>
+      {/* header */}
       <HomeHeader />
+
+      {/* body */}
+      <div className="flex flex-col items-center mt-24">
+        <Image
+          src="https://logosmarcas.net/wp-content/uploads/2020/09/Google-Logo.png"
+          alt="logo"
+          width="300"
+          height="100"
+        />
+
+        <HomeSearch />
+      </div>
     </>
   );
 }
