194226da0d23894fcf95d26b41ff6141bee92b30
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-11-14 13:45:17 -0300

    Create search result component

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,7 +1,9 @@
-import Link from 'next/link';
 import React from 'react';
+import Link from 'next/link';
+import WebSearchResults from '@/components/WebSearchResults';
 
 export default async function WebSearchPage({ searchParams }) {
+  // await new Promise((resolve) => setTimeout(resolve, 10000));
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
   );
@@ -27,10 +29,5 @@ export default async function WebSearchPage({ searchParams }) {
     );
   }
 
-  return (
-    <div>
-      {results &&
-        results.map((result) => <h1 key={result.title}>{result.title}</h1>)}
-    </div>
-  );
+  return <div>{results && <WebSearchResults results={data} />}</div>;
 }
